7818881d52ec2b1498ebc341da6f88a4c6eb4efb
rusher <rusher315@sina.com>
2016-05-09 18:43:25 +0800

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import com.one.HttpRequest;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String s= HttpRequest.sendGet("http://www.baidu.com", "");
+        System.out.println(s);
     }
 }
